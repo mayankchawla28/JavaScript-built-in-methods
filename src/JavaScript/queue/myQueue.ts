981b4b5ac287578daa5b656ed3ba38c214c88800
mayank <mayankchawla99@live.in>
2024-05-13 01:34:51 +0530

    queue's methods and property.

--- src/JavaScript/queue/myQueue.ts
+++ src/JavaScript/queue/myQueue.ts
@@ -3,5 +3,18 @@ class MyQueue {
   constructor (itemArray = []) {
     this.items = itemArray;
   }
-  
+
+  enqueue() {}
+
+  dequeue() {}
+
+  front() {}
+
+  isEmpty() {}
+
+
+  get size() {
+    // Return the number of elements in the queue
+    return this.items.length;
+  }
 }
