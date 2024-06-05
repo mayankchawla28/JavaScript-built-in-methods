2d6f3a4c2947dd77d5f688d8abd1291214e09a0e
mayank <mayankchawla99@live.in>
2024-06-05 23:39:26 +0530

    priority queue front method.

--- src/JavaScript/priority-queue/myPriorityQueue.ts
+++ src/JavaScript/priority-queue/myPriorityQueue.ts
@@ -25,7 +25,12 @@ class MyPriorityQueue {
     return this.items.shift();
   }
 
-  front() {}
+  front() {
+    if (this.isEmpty()) {
+      return "No elements in Queue.";
+    }
+    return this.items[0];
+  }
 
   isEmpty() {
     return this.items.length === 0;
