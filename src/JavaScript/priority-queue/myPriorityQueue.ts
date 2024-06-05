fd15dbfa0f87c12732cd554598fa9554714d05bf
mayank <mayankchawla99@live.in>
2024-06-05 23:33:12 +0530

    priority queue enqueue method and property value & size.

---
+++ src/JavaScript/priority-queue/myPriorityQueue.ts
@@ -0,0 +1,34 @@
+class MyPriorityQueue {
+  items;
+  constructor(itemArray = []) {
+    this.items = itemArray;
+  }
+
+  enqueue(element, priority) {
+    let isAdded = false;
+    for (let i = 0; i < this.size(); i++) {
+      if (this.items[i].priority < priority) {
+        this.items.splice(i, 0, { element, priority });
+        isAdded = true;
+        break;
+      }
+    }
+    if (!isAdded) {
+      this.items.push({ element, priority });
+    }
+  }
+
+  dequeue() {}
+
+  front() {}
+
+  isEmpty() {}
+
+  get value() {
+    return this.items;
+  }
+
+  get size() {
+    return this.items.length;
+  }
+}
