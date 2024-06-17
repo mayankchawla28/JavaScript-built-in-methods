0edbf99bdc815fe9cf5dc5f4aa8944a6da7290b6
mayank <mayankchawla99@live.in>
2024-06-17 23:46:06 +0530

    myHashTable class

---
+++ src/JavaScript/hash-tables/myHashTable.ts
@@ -0,0 +1,15 @@
+class myHashTable {
+  table;
+  size;
+  constructor() {
+    this.table = new Array(11);
+    this.size = 0;
+  }
+
+  
+}
+
+(function main() {
+  const hs = new myHashTable();
+  console.log("hs", hs);
+})();
